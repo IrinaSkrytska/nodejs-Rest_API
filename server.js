1d0ea8cfac8e6f163566db8b1f14dd72ee751740
Iryna <93982819+IrinaSkrytska@users.noreply.github.com>
2022-10-12 14:20:51 +0300

    rewrites using Mongoose

--- server.js
+++ server.js
@@ -5,13 +5,21 @@ const mongoose = require("mongoose");
 const PORT = process.env.PORT || 3000;
 const DB_HOST = process.env.MONGO_URL;
 
-mongoose
-  .connect(DB_HOST)
+const connection = mongoose.connect(DB_HOST, {
+  promiseLibrary: global.Promise,
+  // useCreateIndex: true,
+  useUnifiedTopology: true,
+  // useFindAndModify: false,
+});
+
+connection
   .then(() => {
-    console.log("Database connection is successful");
-    app.listen(PORT);
+    console.log("Database connection successful");
+    app.listen(PORT, function () {
+      console.log(`Server running. Use our API on port: ${PORT}`);
+    });
   })
-  .catch((error) => {
-    console.log(`Server is not running. Error message: ${error.message}`);
+  .catch((err) => {
+    console.log(`Server not running. Error message: ${err.message}`);
     process.exit(1);
   });
