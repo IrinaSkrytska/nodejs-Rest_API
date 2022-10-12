06a12c74d1102044daf12fe1ea64e09aaa553ff4
Iryna <93982819+IrinaSkrytska@users.noreply.github.com>
2022-10-12 10:23:02 +0300

    adds schema

---
+++ middlewares/schemas.js
@@ -0,0 +1,18 @@
+const { Schema, model } = require("mongoose");
+
+const contact = new Schema({
+  name: {
+    type: String,
+    required: [true, "Set name for contact"],
+  },
+  email: {
+    type: String,
+  },
+  phone: {
+    type: String,
+  },
+  favorite: {
+    type: Boolean,
+    default: false,
+  },
+});
--- server.js
+++ server.js
@@ -1,5 +1,17 @@
-const app = require('./app')
+require("dotenv").config();
+const app = require("./app");
+const mongoose = require("mongoose");
 
-app.listen(3000, () => {
-  console.log("Server running. Use our API on port: 3000")
-})
+const PORT = process.env.PORT || 3000;
+const DB_HOST = process.env.MONGO_URL;
+
+mongoose
+  .connect(DB_HOST)
+  .then(() => {
+    console.log("Database connection is successful");
+    app.listen(PORT);
+  })
+  .catch((error) => {
+    console.log(`Server is not running. Error message: ${error.message}`);
+    process.exit(1);
+  });
